1f7bf4a5ddc10dc559506dbe69e55d65052c6212
MarceloCesar <jsdevmarcelo@gmail.com>
2023-04-05 06:47:01 -0100

    dart project created

---
+++ learn_math_programming/bin/learn_math_programming.dart
@@ -0,0 +1,5 @@
+import 'package:learn_math_programming/learn_math_programming.dart' as learn_math_programming;
+
+void main(List<String> arguments) {
+  print('Hello world: ${learn_math_programming.calculate()}!');
+}
